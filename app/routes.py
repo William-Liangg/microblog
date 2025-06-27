425f1f9be3e8fe92ae8ce09837ce299010a52f66
William-Liangg <142444196+William-Liangg@users.noreply.github.com>
2025-06-27 15:31:03 -0400

    fixed bugs with login directory, added secret key

--- app/routes.py
+++ app/routes.py
@@ -17,7 +17,7 @@ def index():
         }
     ]
     return render_template('index.html', title='Home', user=user, posts=posts)
-    
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
@@ -26,3 +26,5 @@ def login():
             form.username.data, form.remember_me.data))
         return redirect('/index')
     return render_template('login.html', title='Sign In', form=form)
+    
+    
