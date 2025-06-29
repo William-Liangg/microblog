212690d329d40db1eed2a44d1f4f05f46403765e
William-Liangg <142444196+William-Liangg@users.noreply.github.com>
2025-06-29 02:23:03 -0400

    added pagination to home page and user profile

--- app/routes.py
+++ app/routes.py
@@ -19,8 +19,26 @@ def index():
         db.session.commit()
         flash('Your post is now live!')
         return redirect(url_for('index'))
-    posts = db.session.scalars(current_user.following_posts()).all()
-    return render_template('index.html', title='Home Page', form=form, posts=posts)
+    page = request.args.get('page', 1, type=int)
+    posts = db.paginate(current_user.following_posts(), page=page, per_page=app.config['POSTS_PER_PAGE'], error_out=False) # paginated to limit the amount of posts loaded per page
+    next_url = url_for('index', page=posts.next_num) \
+        if posts.has_next else None
+    prev_url = url_for('index', page=posts.prev_num) \
+        if posts.has_prev else None
+    return render_template('index.html', title='Home', form=form, posts=posts.items, next_url=next_url, prev_url=prev_url)
+
+@app.route('/explore')
+@login_required
+def explore():
+    page = request.args.get('page', 1, type=int)
+    query = sa.select(Post).order_by(Post.timestamp.desc())
+    posts = db.paginate(query, page=page, per_page=app.config['POSTS_PER_PAGE'], error_out=False) # paginated
+    next_url = url_for('explore', page=posts.next_num) \
+        if posts.has_next else None
+    prev_url = url_for('explore', page=posts.prev_num) \
+        if posts.has_prev else None
+    return render_template('index.html', title='Explore', posts=posts)
+
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -61,13 +79,16 @@ def register():
 @app.route('/user/<username>')
 @login_required
 def user(username):
-    user = db.session.scalar(sa.select(User).where(User.username == username))
-    if user is None:
-        abort(404)
-    posts = [
-        {'author': user, 'body': 'Test post #1'},
-        {'author': user, 'body': 'Test post #2'}
-    ]
+    user = db.first_or_404(sa.select(User).where(User.username == username))
+    page = request.args.get('page', 1, type=int) # pagination in the user profile view 
+    query = user.posts.select().order_by(Post.timestamp.desc())
+    posts = db.paginate(query, page=page,
+                        per_page=app.config['POSTS_PER_PAGE'],
+                        error_out=False)
+    next_url = url_for('user', username=user.username, page=posts.next_num) \
+        if posts.has_next else None
+    prev_url = url_for('user', username=user.username, page=posts.prev_num) \
+        if posts.has_prev else None
     # to render the follow button
     form = EmptyForm()
     return render_template('user.html', user=user, posts=posts, form=form)
@@ -135,9 +156,3 @@ def unfollow(username):
     else:
         return redirect(url_for('index'))
 
-@app.route('/explore')
-@login_required
-def explore():
-    query = sa.select(Post).order_by(Post.timestamp.desc())
-    posts = db.session.scalars(query).all()
-    return render_template('index.html', title='Explore', posts=posts)
